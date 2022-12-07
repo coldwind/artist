1cc0ad04bce4a97fae8dcc5a22e5c1a229f86af7
machao <vegeta.ma@163.com>
2022-12-08 00:09:00 +0800

    添加配置读取&缓存功能

---
+++ pkg/icfg/go.mod
@@ -0,0 +1,18 @@
+module github.com/coldwind/artist/pkg/icfg
+
+go 1.19
+
+require (
+	github.com/coldwind/artist/pkg/ilog v0.0.0-20221206143241-eea9edf6031c
+	github.com/json-iterator/go v1.1.12
+	go.uber.org/zap v1.24.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
---
+++ pkg/icfg/icfg.go
@@ -0,0 +1,71 @@
+package icfg
+
+import (
+	"errors"
+	"os"
+	"sync"
+
+	"github.com/coldwind/artist/pkg/ilog"
+	jsoniter "github.com/json-iterator/go"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
+)
+
+type CfgType int
+
+type service struct {
+	sync.RWMutex
+	cache map[string]interface{}
+}
+
+var (
+	CfgTypeYaml CfgType = 1
+	CfgTypeJson CfgType = 2
+)
+
+var handle *service
+
+func init() {
+	handle = &service{}
+}
+
+func Register(t CfgType, key, path string, data interface{}) error {
+	cfg, err := os.ReadFile(path)
+	if err != nil {
+		ilog.Error("get cfg file error", zap.String("path", path), zap.Error(err))
+	}
+
+	err = errors.New("register failure")
+	if t == CfgTypeYaml {
+		err = yaml.Unmarshal(cfg, data)
+		if err != nil {
+			ilog.Error("yaml Unmarshal file error", zap.String("path", path), zap.Error(err))
+		}
+	} else if t == CfgTypeJson {
+		err = jsoniter.Unmarshal(cfg, data)
+		if err != nil {
+			ilog.Error("json Unmarshal file error", zap.String("path", path), zap.Error(err))
+		}
+	}
+
+	if err != nil {
+		return err
+	}
+
+	handle.Lock()
+	defer handle.Unlock()
+
+	handle.cache[key] = data
+
+	return nil
+}
+
+func Get(key string) interface{} {
+	handle.RLock()
+	defer handle.RUnlock()
+	if _, ok := handle.cache[key]; ok {
+		return handle.cache[key]
+	}
+
+	return nil
+}
