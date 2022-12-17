61a0c6309239da08aba0073779e009105ed7671a
machao <vegeta.ma@163.com>
2022-12-17 21:23:12 +0800

    install

--- gencode/install.sh
+++ gencode/install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 export LC_CTYPE=C 
 export LANG=C
-if [ $# -eq 1 ]; then
-    grep -rl --exclude=install.sh . | xargs sed -i "" "s/ARTIST_PROJECT_NAME/${1}/g"
+if [ $# -eq 2 ]; then
+    `rm -rf $2/$1`
+    `cp -rf ../gencode $2/$1`
+    `rm -f $2/$1/install.sh`
+    `cd $2/$1 && grep -rl --exclude=install.sh . | xargs sed -i "" "s/ARTIST_PROJECT_NAME/${1}/g"`
 else
-    echo "项目名不能为空."
+    echo "项目名和项目路径不能为空."
 fi
