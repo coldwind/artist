2f64015224d8cc60c20612d5c89c6aa55196dfed
machao <vegeta.ma@163.com>
2022-12-22 23:37:04 +0800

    增加int转换方法

--- pkg/iutils/math.go
+++ pkg/iutils/math.go
@@ -11,6 +11,24 @@ func StringToInt64(num string) (int64, error) {
 	return dec.IntPart(), nil
 }
 
+func StringToInt32(num string) (int32, error) {
+	dec, err := decimal.NewFromString(num)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(dec.IntPart()), nil
+}
+
+func StringToInt(num string) (int, error) {
+	dec, err := decimal.NewFromString(num)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(dec.IntPart()), nil
+}
+
 func StringToFloat64(num string) float64 {
 	dec, err := decimal.NewFromString(num)
 	if err != nil {
