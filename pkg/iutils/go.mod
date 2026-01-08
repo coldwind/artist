0d43b3d4c1cf25b60785bf69c33919d79faa247c
machao <vegeta.ma@163.com>
2026-01-08 18:33:12 +0800

    增加日期转unix时间戳方法

--- pkg/iutils/go.mod
+++ pkg/iutils/go.mod
@@ -1,5 +1,16 @@
 module github.com/coldwind/artist/pkg/iutils
 
-go 1.19
+go 1.24.0
 
-require github.com/shopspring/decimal v1.3.1 // indirect
+toolchain go1.24.2
+
+require (
+	github.com/shopspring/decimal v1.3.1
+	github.com/valyala/fasthttp v1.69.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
--- pkg/iutils/time.go
+++ pkg/iutils/time.go
@@ -1,6 +1,8 @@
 package iutils
 
-import "time"
+import (
+	"time"
+)
 
 // Sec 秒时间戳
 func Sec() int64 {
@@ -48,4 +50,19 @@ func TimeZero() int64 {
 	t := time.Now()
 	t1 := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 	return t1.Unix()
+}
+
+// 格式日期转unix时间戳
+func TimeStrToUnix(timeStr string, loc *time.Location) (int64, error) {
+	// 定义时间字符串的格式，必须和输入字符串完全匹配
+	layout := "2006-01-02 15:04:05"
+
+	// 解析时间字符串为 time.Time 类型
+	t, err := time.ParseInLocation(layout, timeStr, loc)
+	if err != nil {
+		return -1, err
+	}
+
+	// 转换为 Unix 时间戳（秒级）
+	return t.Unix(), nil
 }
